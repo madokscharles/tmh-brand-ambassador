4268a6b65aa1a324ad7899da8441fcb1eb69a523
madokscharles <charlesmadoks@gmail.com>
2024-07-19 02:58:17 +0100

    make migrations for models

--- model_competition/models_app/admin.py
+++ model_competition/models_app/admin.py
@@ -1,3 +1,9 @@
+# models_app/admin.py
+
 from django.contrib import admin
+from .models import Profile, Competition, Entry
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Competition)
+admin.site.register(Entry)
--- model_competition/models_app/models.py
+++ model_competition/models_app/models.py
@@ -1,3 +1,33 @@
+# models_app/models.py
+
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
+
+class Competition(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+
+class Entry(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    competition = models.ForeignKey(Competition, on_delete=models.CASCADE)
+    entry_picture = models.ImageField(upload_to='entry_pictures/')
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.profile.user.username} - {self.competition.name}"
